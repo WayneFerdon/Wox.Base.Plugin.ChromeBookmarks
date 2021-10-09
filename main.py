75f86874691219cccdfb0404a9ca9308bf58b317
wayneferdon <wayneferdon@hotmail.com>
2021-10-10 03:44:04 +0800

    fix PermissionError while loading icons in wox-1.4.1196

--- main.py
+++ main.py
@@ -33,8 +33,11 @@ class regexList:
 def createIcon(bitmapInfoList):
     for iconId in bitmapInfoList.keys():
         imageData = bitmapInfoList[iconId]['imageData']
-        with open('./Images/iconId{}.png'.format(iconId), 'wb') as f:
-            f.write(imageData)
+        try:
+            with open('./Images/iconId{}.png'.format(iconId), 'wb') as f:
+                f.write(imageData)
+        except PermissionError:
+            pass
 
 
 def makeList(itemsList, childItems, pathFolder):
