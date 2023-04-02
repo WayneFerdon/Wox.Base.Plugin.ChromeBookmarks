a135bd1b965567d1e8721bbfc85fccad7cf118d9
WayneFerdon <wayneferdon@hotmail.com>
2023-04-03 02:31:55 +0800

    extra common files for further resue

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Author: wayneferdon wayneferdon@hotmail.com
 # Date: 2022-02-12 06:25:49
 # LastEditors: WayneFerdon wayneferdon@hotmail.com
-# LastEditTime: 2023-03-04 13:47:57
+# LastEditTime: 2023-04-03 01:03:29
 # FilePath: \Flow.Launcher.Plugin.ChromeBookmarks\main.py
 # ----------------------------------------------------------------
 # Copyright (c) 2022 by Wayne Ferdon Studio. All rights reserved.
@@ -12,35 +12,42 @@
 # ----------------------------------------------------------------
 
 # -*- coding: utf-8 -*-
-from ChromeQuery import *
+import sys
+import os
+import json
 
-class GetBookmarks(ChromeQuery):
-    with(
-        open('./plugin.json','r') as pluginJson,
-        open(Query.SettingPath,'r') as settingJson
-    ):
-        plugInID = json.load(pluginJson)['ID']
-        __actionKeyword__ = json.load(settingJson)['PluginSettings']['Plugins'][plugInID]['ActionKeywords'][0]
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from WoxPluginBase_ChromeQuery import *
+
+class BookmarksQuery(ChromeQuery):
+    __actionKeyword__ = None
 
     def _getDatas_(self):
-        return Cache.getBookmarks()
+        return ChromeCache.getBookmarks()
 
     def _getResult_(self, regex:RegexList, data:Bookmark):
-        item = data.title + ';' + data.path + ';' + data.url + '/'
+        BookmarksQuery.__actionKeyword__ = None
+        if BookmarksQuery.__actionKeyword__ is None:
+            with(
+                open('./plugin.json','r') as pluginJson
+            ):
+                plugInID = json.load(pluginJson)['ID']
+                BookmarksQuery.__actionKeyword__ = BookmarksQuery.GetActionKeyword(plugInID)
+
+        item = f'{data.platform.name};{data.title};{data.path};{data.url}/'
         if not regex.match(item):
             return
         match data.type:
             case Bookmark.Type.url:
-                return QueryResult(data.title, data.url, data.icon, self._datas_.index(data), self._openUrl_.__name__, True, data.url).toDict()
+                return QueryResult(data.platform.name + ' ' + data.title, data.url, data.icon, self._datas_.index(data), self._openUrl_.__name__, True, data.url).toDict()
             case Bookmark.Type.folder:
                 if data.url == regex.queryString:
                     # if right in the quering folder, not return it
                     return
-                return QueryResult(data.title, data.url, data.icon, self._datas_.index(data), Launcher.GetAPIName(Launcher.API.ChangeQuery), False, GetBookmarks.__actionKeyword__ + ' ' + data.url, True).toDict()
+                return QueryResult(data.platform.name + ' '  + data.title, data.url, data.icon, self._datas_.index(data), Launcher.GetAPIName(Launcher.API.ChangeQuery), False, BookmarksQuery.__actionKeyword__ + ' ' + data.url, True).toDict()
 
     def _extraContextMenu_(self, data:Bookmark):
         return [self.getCopyDataResult('Directory', data.directory, ChromeData.FOLDER_ICON)]
 
 if __name__ == '__main__':
-    GetBookmarks()
-    # GetBookmarks().query('other/')
+    BookmarksQuery()
